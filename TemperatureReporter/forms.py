36fd32c1f46f574fe824aea57d43b210d27f9d55
李峰 <lifeng.wuhan@gmail.com>
2020-02-16 12:47:09 +0800

    增加记录员权限校验逻辑

--- TemperatureReporter/forms.py
+++ TemperatureReporter/forms.py
@@ -6,22 +6,25 @@ class EmployeeTemperatureSubmitForm(forms.Form):
     # sessionId = forms.CharField(max_length=30)
     teamId = forms.CharField(max_length=50)
     employeeId = forms.CharField(max_length=15)
-    # employeeName = forms.CharField(max_length=50)
+    employeeName = forms.CharField(max_length=50, required=False)
     temperature = forms.CharField(max_length=5, required=False)
     measureTimes = forms.CharField(max_length=11)
     recorderId = forms.CharField(max_length=15)
-    # recorderName = forms.CharField(max_length=50)
+    recorderName = forms.CharField(max_length=50, required=False)
     remark = forms.CharField(max_length=512, required=False)
+
 
 class TeamTemperatureSubmitForm(forms.Form):
     # sessionId = forms.CharField(max_length=30)
     teamId = forms.CharField(max_length=50)
     teamName = forms.CharField(max_length=50)
+    recorderId = forms.CharField(max_length=15)
     measureTimes = forms.CharField(max_length=11)
 
 
 class QueryTeamTemperatureRecordsForm(forms.Form):
     # sessionId = forms.CharField(max_length=30)
     teamId = forms.CharField(max_length=50)
+    recorderId = forms.CharField(max_length=15)
     measureDate = forms.CharField(required=False)
     measureTimes = forms.CharField(max_length=11)
